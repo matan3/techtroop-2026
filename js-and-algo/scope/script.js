e1084c0987172797669054f1374334b542adefe6
Matan <matan4491@gmail.com>
2026-06-04 16:16:54 +0300

    Add JavaScript exercises for scope errors and variable assignments

---
+++ js-and-algo/scope/script.js
@@ -0,0 +1,26 @@
+// Exercise 1
+// ReferenceError: distance is not defined
+// because distance is outside
+
+// Exercise 2
+// ReferenceError: cowSound is not defined
+// because cowSound in a different scope
+
+// Exercise 3
+// working
+// Served a special fish
+// Served a special lettuce plate
+// Served a special curious cheese
+// Finished serving all the orders: fish,lettuce plate,curious cheese
+
+// Exercise 4
+// ReferenceError: seed is not defined
+// seed in a different scope
+
+// Exercise 5
+// ReferenceError: found is not defined
+// found is only inside the loop
+
+// Exercise 6
+// TypeError: Assignment to constant variable.
+// cant cahnge constant variable
