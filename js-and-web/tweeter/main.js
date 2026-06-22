c3c4623f3f3c11d0414ba7b2160f65d063c39e64
Matan <matan4491@gmail.com>
2026-06-22 16:02:54 +0300

    Step 4 finalized

--- js-and-web/tweeter/main.js
+++ js-and-web/tweeter/main.js
@@ -5,3 +5,33 @@ const tweeter = Tweeter();
 const renderer = Renderer();
 
 renderer.renderPosts(tweeter.getPosts());
+
+$("#twit").on("click", function () {
+    const inputValue = $("#input").val();
+    tweeter.addPost(inputValue);
+    $("#input").val("");
+    renderer.renderPosts(tweeter.getPosts());
+});
+
+$("#posts").on("click", ".delete", function () {
+    const postId = $(this).data("id");
+    tweeter.removePost(postId);
+    renderer.renderPosts(tweeter.getPosts());
+});
+
+$("#posts").on("click", ".comment-button", function () {
+    const postElement = $(this).closest(".post");
+    const postId = postElement.data("id");
+    const commentText = postElement.find(".comment-input").val();
+
+    tweeter.addComment(postId, commentText);
+    postElement.find(".comment-input").val("");
+    renderer.renderPosts(tweeter.getPosts());
+});
+
+$("#posts").on("click", ".delete-comment", function () {
+    const commentId = $(this).data("id");
+    const postId = $(this).closest(".post").data("id");
+    tweeter.removeComment(postId, commentId);
+    renderer.renderPosts(tweeter.getPosts());
+});
